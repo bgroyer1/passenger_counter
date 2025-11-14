b65c84b2163969485fd8053b3b17d4c6fa39d21a
Braedan Royer <royerbraedan@gmail.com>
2025-11-14 15:48:41 -0800

    created the increment count function. set up the count variable. count now increments when you click the button.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-  
+let count = 0;
+const countEl = document.querySelector('#count-el')
+const incrementBtn = document.querySelector('#increment-btn')
+
+function incrementCount() {
+  count++
+  countEl.innerText = count
+}
+
+incrementBtn.addEventListener('click', incrementCount )
+
