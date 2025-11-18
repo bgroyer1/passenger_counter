bf94dece3bce94c55bb0b3dcfa90624b272e8b22
Braedan Royer <royerbraedan@gmail.com>
2025-11-17 19:39:00 -0800

    added basic styling

--- js/index.js
+++ js/index.js
@@ -1,11 +1,22 @@
 let count = 0;
-const countEl = document.querySelector('#count-el')
-const incrementBtn = document.querySelector('#increment-btn')
+let totalCount = 0;
+const countEl = document.querySelector("#count-el");
+const incrementBtn = document.querySelector("#increment-btn");
+const saveBtn = document.querySelector("#save-btn");
+const prevCountSpan = document.querySelector("#prev-count-p");
+const totalCountSpan = document.querySelector('#total-count-p')
 
 function incrementCount() {
-  count++
-  countEl.innerText = count
+  count++;
+  countEl.innerText = count;
 }
 
-incrementBtn.addEventListener('click', incrementCount )
+function save() {
+  prevCountSpan.innerText += ` ${count} - `;
+  totalCount += count;
+  totalCountSpan.innerText = `Total Count: ${totalCount}`;
+  count = 0;
+}
 
+incrementBtn.addEventListener("click", incrementCount);
+saveBtn.addEventListener("click", save);
